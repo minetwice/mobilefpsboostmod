c70395b67adf7b69b98b3219221ee99df0aed9ce
Twicefear <ytd82774@gmail.com>
2026-05-04 18:58:13 +0530

    Initial commit of mobile FPS boost mod files

---
+++ src/main/java/com/rampurhat/mobileboost/mixin/EntityRendererMixin.java
@@ -0,0 +1,1 @@
+package com.rampurhat.mobileboost.mixin; import org.spongepowered.asm.mixin.Mixin; import net.minecraft.client.render.entity.EntityRenderer; @Mixin(EntityRenderer.class) public class EntityRendererMixin {}
