0cb255d39c998b1cd8f5fc280b4355af359ead35
Twicefear <ytd82774@gmail.com>
2026-05-10 00:08:03 +0530

    Update Android and Fabric Loom plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // MinecraftMobileOptimizer/build.gradle.kts
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("fabric-loom") version "1.8-SNAPSHOT" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("fabric-loom") version "1.9.2" apply false
 }
 
 allprojects {
