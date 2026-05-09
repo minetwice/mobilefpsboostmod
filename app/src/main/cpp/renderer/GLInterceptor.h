aa1f69fed4526231cdadd5a5b3b297847a3e90db
Twicefear <ytd82774@gmail.com>
2026-05-09 21:55:54 +0530

    Update GLInterceptor.h

--- app/src/main/cpp/renderer/GLInterceptor.h
+++ app/src/main/cpp/renderer/GLInterceptor.h
@@ -1,41 +1,17 @@
-#ifndef GL_INTERCEPTOR_H
-#define GL_INTERCEPTOR_H
+// MinecraftMobileOptimizer/app/src/main/cpp/renderer/GLInterceptor.h
+#pragma once
 
-#include <GLES3/gl3.h>
-#include <string>
-
-namespace MCOptimizer {
-
-class GLInterceptor {
-public:
-    static GLInterceptor& getInstance();
-    
-    void initialize();
-    void interceptGLCalls();
-    
-    // Intercepted OpenGL functions
-    void glDrawArrays(GLenum mode, GLint first, GLsizei count);
-    void glDrawElements(GLenum mode, GLsizei count, GLenum type, const void* indices);
-    void glTexImage2D(GLenum target, GLint level, GLint internalformat, 
-                      GLsizei width, GLsizei height, GLint border, 
-                      GLenum format, GLenum type, const void* pixels);
-    
-    // Performance tracking
-    int getDrawCallCount() const { return m_drawCallCount; }
-    int getTextureCount() const { return m_textureCount; }
-    void resetCounters();
-    
-private:
-    GLInterceptor() = default;
-    ~GLInterceptor() = default;
-    GLInterceptor(const GLInterceptor&) = delete;
-    GLInterceptor& operator=(const GLInterceptor&) = delete;
-    
-    int m_drawCallCount = 0;
-    int m_textureCount = 0;
-    bool m_initialized = false;
-};
-
-} // namespace MCOptimizer
-
-#endif // GL_INTERCEPTOR_H
+namespace GLInterceptor {
+void initialize();
+void shutdown();
+void onDrawArrays(int mode, int first, int count);
+void onDrawElements(int mode, int count, int type, const void* indices);
+void onBindTexture(unsigned int texture);
+void onUseProgram(unsigned int program);
+void setCullingEnabled(bool enabled);
+void setBatchingEnabled(bool enabled);
+void setLODEnabled(bool enabled);
+int getDrawCallCount();
+int getVertexCount();
+void resetStats();
+}
