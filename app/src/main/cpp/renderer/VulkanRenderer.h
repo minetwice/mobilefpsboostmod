68e2cfbb79ed1e17aa9d88a41882e5e96241aed2
Twicefear <ytd82774@gmail.com>
2026-05-09 21:53:47 +0530

    Update VulkanRenderer.h

--- app/src/main/cpp/renderer/VulkanRenderer.h
+++ app/src/main/cpp/renderer/VulkanRenderer.h
@@ -1,46 +1,20 @@
-#ifndef VULKAN_RENDERER_H
-#define VULKAN_RENDERER_H
-
-#include <vulkan/vulkan.h>
-#include <string>
-#include <vector>
-
-namespace MCOptimizer {
+// MinecraftMobileOptimizer/app/src/main/cpp/renderer/VulkanRenderer.h
+#pragma once
+#include <memory>
 
 class VulkanRenderer {
 public:
-    static VulkanRenderer& getInstance();
-    
+    VulkanRenderer();
+    ~VulkanRenderer();
     bool initialize();
-    void destroy();
-    
-    void setResolution(int width, int height);
-    void swapBuffers();
-    
-    bool isInitialized() const { return m_initialized; }
-    
+    void shutdown();
+    void setRenderScale(float scale);
+    float getRenderScale() const;
+    void beginRenderPass();
+    void endRenderPass();
+    void interceptGLDrawArrays(int mode, int first, int count);
+    void interceptGLDrawElements(int mode, int count, int type, const void* indices);
 private:
-    VulkanRenderer() = default;
-    ~VulkanRenderer() = default;
-    VulkanRenderer(const VulkanRenderer&) = delete;
-    VulkanRenderer& operator=(const VulkanRenderer&) = delete;
-    
-    bool createInstance();
-    bool createDevice();
-    bool createSwapchain();
-    void cleanupSwapchain();
-    
-    VkInstance m_instance = VK_NULL_HANDLE;
-    VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
-    VkDevice m_device = VK_NULL_HANDLE;
-    VkSurfaceKHR m_surface = VK_NULL_HANDLE;
-    VkSwapchainKHR m_swapchain = VK_NULL_HANDLE;
-    
-    int m_width = 0;
-    int m_height = 0;
-    bool m_initialized = false;
+    class Impl;
+    std::unique_ptr<Impl> pImpl;
 };
-
-} // namespace MCOptimizer
-
-#endif // VULKAN_RENDERER_H
