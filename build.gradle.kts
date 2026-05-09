53fdcaf092e693ec988c2db9bf22c8ad15c608ed
Twicefear <ytd82774@gmail.com>
2026-05-09 23:07:03 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // MinecraftMobileOptimizer/build.gradle.kts
 plugins {
     id("com.android.application") version "8.3.0" apply false
-    id("fabric-loom") version "1.6-SNAPSHOT" apply false
+    id("fabric-loom") version "1.7-SNAPSHOT" apply false
 }
 
 allprojects {
@@ -9,6 +9,5 @@ allprojects {
         google()
         mavenCentral()
         maven("https://maven.fabricmc.net/")
-        maven("https://maven.minecraftforge.net/")
     }
 }
